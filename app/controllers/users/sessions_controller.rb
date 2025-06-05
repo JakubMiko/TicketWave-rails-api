777b391690a8177eb15b01166dac0573f0f60e4a
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-06-05 14:37:00 +0200

    fix: update header and user login to ViewComponent

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,14 +4,32 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    respond_to do |format|
+      format.html do
+        render Devise::Users::Sessions::FormComponent.new(
+          resource: resource,
+          resource_name: resource_name,
+          devise_mapping: devise_mapping
+        )
+      end
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "modal-alert",
+          Ui::AlertComponent.new(
+            description: flash[:alert],
+            variant: :error
+          ).render_in(view_context)
+        )
+      end
+    end
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
   def destroy
