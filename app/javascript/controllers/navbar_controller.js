b0d66df766e636aa37efb21bc107815763a62f03
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-07-30 11:23:33 +0200

    add hamburger menu in navbar for mobile devices

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,15 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["menu", "burger"];
+  static targets = ["menu"];
 
   connect() {
     console.log("Navbar controller connected");
   }
 
   toggle() {
-    // Dodaj lub usuń klasę "is-active" dla burgera i menu
-    this.burgerTarget.classList.toggle("is-active");
-    this.menuTarget.classList.toggle("is-active");
+    this.menuTarget.classList.toggle("hidden");
   }
 }
