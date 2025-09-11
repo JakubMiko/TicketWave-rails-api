803d6806a8c4f84e77b10d587f477afd8668e182
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-09-11 14:58:21 +0200

    refactor confirmation and fix redirect after successful order creation

--- app/components/base_component.rb
+++ app/components/base_component.rb
@@ -1,4 +1,5 @@
 class BaseComponent < ViewComponent::Base
   # This is a base component that can be extended by other components.
   # It can include shared methods or helpers that are common across components.
+  delegate :user_signed_in?, :admin_signed_in?, to: :helpers
 end
