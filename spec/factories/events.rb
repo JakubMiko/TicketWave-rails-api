6bc209cf62f7cfa87edd47cbb8aa609a4a204b21
Jakub Mikołajczyk <kubamiko11@gmail.com>
2025-09-19 15:11:55 +0200

    add trait to events factory bot to create past and upcomming events in specs

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     category { "music" }
     date { DateTime.now + 5.days }
   end
+
+  trait :upcoming do
+    date { DateTime.now + 5.days }
+  end
+
+  trait :past do
+    date { DateTime.now - 5.days }
+  end
 end
